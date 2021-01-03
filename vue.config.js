02065507723010d0d78a0bcd933fcbc523484286
八代秀明 <yashirohideaki@yashirohideakinoMacBook-Pro.local>
2021-01-03 22:10:45 +0900

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//       ? '/vue-router-tweet-application/'
+//       : '/'
+//   }
 module.exports = {
   outputDir: './docs',
 }
